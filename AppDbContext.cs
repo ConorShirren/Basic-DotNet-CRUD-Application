77bd83abed515a79b89e08cbaa24e347bf0b3370
conorshirren <cshirren@gmail.com>
2020-02-05 16:31:38 +0000

    Initial Commit of Project

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mywebapp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mywebapp
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(10)]
+        public string Name { get; set; }
+
+
+
+    }
+}
